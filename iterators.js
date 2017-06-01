8ccb295a72cc9ff885a24aadd2977f89f17d89b1
mvciekzvbek <maciej.jan.zabek@gmail.com>
2017-06-01 15:13:56 +0200

    iterators

---
+++ iterators.js
@@ -0,0 +1,26 @@
+let person = {
+	name: "Maciej",
+	surname: "Zabek",
+	age: 23,
+	profession: "Programmer"
+}
+
+person[Symbol.iterator] = function(){
+	let properties = Object.keys(this);
+	let count = 0;
+	let isDone = false;
+
+	let next = () => {
+		if (count >= properties.length) isDone = true;
+		return { done: isDone, value: this[properties[count++]] };
+	}
+
+	return { next };
+}
+
+for (let properties of person) {
+	console.log(properties);
+}
+
+let values = [...person];
+console.log(values);
