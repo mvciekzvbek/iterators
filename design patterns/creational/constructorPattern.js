76f99b96a1617ebe037b0e8719efe06885bf3069
mvciekzvbek <maciej.jan.zabek@gmail.com>
2018-05-06 17:18:20 +0200

    added two creational design patterns

---
+++ design patterns/creational/constructorPattern.js
@@ -0,0 +1,23 @@
+var Task = function (name) {
+	this.name = name;
+	this.completed = false;
+
+	this.complete = function () {
+		console.log('Completing task: ' + this.name);
+		this.completed = true;
+	};
+
+	this.save = function () {
+		console.log('Saving task: ' + this.name);
+	};
+};
+
+var task1 = new Task('Prepare Constructor Pattern demonstration');
+var task2 = new Task('Prepare Module Pattern demonstration');
+var task3 = new Task('Prepare Singleton Pattern demonstration');
+var task4 = new Task('Prepare Prototype Pattern demonstration');
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
