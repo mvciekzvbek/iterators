1c4bbf190f3161b9ce1a6427aff0602ac6107254
Maciej Ząbek <maciek@maciek.local>
2018-07-24 19:06:09 +0200

    structural - decorator

---
+++ design patterns/structural/decorator-pattern/subObjecting.js
@@ -0,0 +1,44 @@
+var Task = function (name) {
+    this.name = name;
+    this.completed = false;
+}
+
+Task.prototype.complete = function () {
+    console.log('completing task: ' + this.name);
+    this.completed = true;
+}
+
+Task.prototype.save = function () {
+    console.log('saving task: ' + this.name);
+}
+
+var myTask = new Task('Legacy task');
+myTask.complete();
+myTask.save();
+
+var UrgentTask = function (name, priority) {
+	Task.call(this, name);
+	this.priority = priority;
+} 
+
+/**
+ * assigning prototypes to be able to call e.g complete function
+ * 
+ * It could be just assigned but the task prototype would change if the UrgentTask prototype changed
+ */
+UrgentTask.prototype = Object.create(Task.prototype);
+
+UrgentTask.prototype.notify = function () {
+	console.log('notifying important people');
+}
+
+// wrapped save with some additional stuff
+UrgentTask.prototype.save = function () {
+	this.notify();
+    console.log('do special stuff before saving');
+    Task.prototype.save.call(this);
+}
+
+var ut = new UrgentTask('This is urgent', 1);
+ut.complete();
+ut.save();
---
+++ design patterns/structural/decorator-pattern/task.js
@@ -0,0 +1,37 @@
+var Task = function (name) {
+    this.name = name;
+    this.completed = false;
+}
+
+Task.prototype.complete = function () {
+    console.log('completing task: ' + this.name);
+    this.completed = true;
+}
+
+Task.prototype.save = function () {
+    console.log('saving task: ' + this.name);
+}
+
+var myTask = new Task('Legacy task');
+myTask.complete();
+myTask.save();
+
+
+var urgentTask = new Task('Urgent Task');
+urgentTask.notify = function () {
+    console.log('notifying important peope');
+}
+
+urgentTask.complete();
+
+/**
+ * Decorated urgent task with new save:
+ * - notifying important people
+ * - call original save from task prototype
+ */
+urgentTask.save = function () {
+    this.notify();
+    Task.prototype.save.call(this);
+}
+
+urgentTask.save();
